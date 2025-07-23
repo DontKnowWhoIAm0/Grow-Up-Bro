1eba4eb54d12a713fa611ba14e36859d69cc45b4
DontKnowWhoIAm0 <sofyakolodeznikova@gmail.com>
2025-07-23 21:20:10 +0300

    Реализовано переключение между кнопками «Поиск по фильтрам» и «Очистить фильтры» на главном экране + исправлено отображение картинок в RecyclerView (добавлены скругленные углы и обводка)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -45,4 +45,5 @@ dependencies {
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
     implementation("com.github.bumptech.glide:glide:4.16.0")
+    implementation("com.google.android.material:material:1.12.0")
 }
--- app/src/main/java/com/example/growupbro/MainScreenFragment.kt
+++ app/src/main/java/com/example/growupbro/MainScreenFragment.kt
@@ -12,35 +12,54 @@ import com.example.growupbro.filters.FiltersFragment
 import com.example.growupbro.testik.StartTestFragment
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.google.android.material.button.MaterialButton
+import androidx.core.view.isVisible
 
 class MainScreenFragment : Fragment(R.layout.fragment_main_screen) {
+
+    private lateinit var adapter: PlantsAdapter
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
         val recyclerView = view.findViewById<RecyclerView>(R.id.recycler_view)
-        recyclerView.layoutManager = StaggeredGridLayoutManager(2, StaggeredGridLayoutManager.VERTICAL)
-        recyclerView.adapter = PlantsAdapter(
+        adapter = PlantsAdapter(
             Glide.with(this),
             { position ->
                 val plant = PlantsRepository.getListForRecyclerView()[position]
                 requireActivity().supportFragmentManager.beginTransaction()
                     .replace(R.id.fragment_container, FullInfoFragment(plant)).addToBackStack(null)
                     .commit()
-                TODO("Пофиксить размеры картинок в FullInfo (хуевит не квадратные, найти способ обрезать)")
             })
+
+        recyclerView.layoutManager = StaggeredGridLayoutManager(2, StaggeredGridLayoutManager.VERTICAL)
+        recyclerView.adapter = adapter
 
         val researchButton = view.findViewById<MaterialButton>(R.id.search)
         researchButton.setOnClickListener {
             val bnm = requireActivity().findViewById<BottomNavigationView>(R.id.menu)
             bnm.visibility = View.GONE
             requireActivity().supportFragmentManager.beginTransaction()
-                .replace(R.id.fragment_container, FiltersFragment())
+                .replace(R.id.fragment_container, FiltersFragment()).addToBackStack(null)
                 .commit()
         }
 
+        val resetFiltersButton = view.findViewById<MaterialButton>(R.id.filtersReset)
+        resetFiltersButton.setOnClickListener {
+            PlantsRepository.shuffleList()
+            onResume()
+        }
     }
 
-
-
+    override fun onResume() {
+        super.onResume()
+        adapter.notifyDataSetChanged()
+        requireActivity().findViewById<BottomNavigationView>(R.id.menu).visibility = View.VISIBLE
+        if (PlantsRepository.getListForRecyclerView().size == PlantsRepository.getFullListOfPlants().size) {
+            requireView().findViewById<MaterialButton>(R.id.search).visibility = View.VISIBLE
+            requireView().findViewById<MaterialButton>(R.id.filtersReset).visibility = View.GONE
+        } else {
+            requireView().findViewById<MaterialButton>(R.id.search).visibility = View.GONE
+            requireView().findViewById<MaterialButton>(R.id.filtersReset).visibility = View.VISIBLE
+        }
+    }
 }
